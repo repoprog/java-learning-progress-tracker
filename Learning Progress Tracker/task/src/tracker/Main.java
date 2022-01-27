4869f6baeff73612f158590dcf87a9d1aa9d9c71
Remigiusz <repogplus@gmail.com>
2022-01-27 22:42:01 +0100

    stage completed

--- Learning Progress Tracker/task/src/tracker/Main.java
+++ Learning Progress Tracker/task/src/tracker/Main.java
@@ -1,9 +1,10 @@
 package tracker;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -25,59 +26,153 @@ public class Main {
                 case "back":
                     System.out.println("Enter 'exit' to exit the program");
                     break;
+                case "list":
+                    showStudentsList();
+                    break;
+                case "add points":
+                    addPoints();
+                    break;
+                case "find":
+                    findStudent();
                 default:
                     System.out.println("Unknown command!");
             }
         }
     }
 
+//    public static void showSubMenu(String info) {
+//
+//    }
+
+    static Scanner scanner = new Scanner(System.in);
+    static Map<Student, Courses> studentResultsMap = new HashMap<>();
+
+
     public static void addStudent() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter student credentials or 'back' to return.");
+        System.out.println("Enter student credentials or 'back' to return:");
         boolean quit = false;
         int students = 0;
-
         while (!quit) {
-            String student = scanner.nextLine().trim();
-            if ("back".equalsIgnoreCase(student)) {
+            String studentInput = scanner.nextLine().trim();
+            if ("back".equalsIgnoreCase(studentInput)) {
                 quit = true;
             } else {
-                if (isPersonalDataValid(student)) {
-                    System.out.println("The student has benn added");
-//                    System.out.print(student);
+                if (isPersonalDataValid(studentInput)) {
+                    System.out.println("The student has benn added.");
                     students++;
                 }
             }
         }
-        System.out.printf("Total %d students have benn added.", students);
+        System.out.printf("Total %d students have been added.\n", students);
     }
 
     public static boolean isPersonalDataValid(String student) {
         Verifier verifier = new Verifier();
         String[] nameParts = student.split(" ");
-        String name = nameParts[0];
-        String lastName;
-        String email;
 
         if (nameParts.length < 3) {
             System.out.println("Incorrect credentials.");
             return false;
-        } else {
-            lastName = String.join(" ", Arrays.copyOfRange(nameParts, 1, nameParts.length - 1));
-            email = nameParts[nameParts.length - 1];
         }
-//        System.out.printf("name: %s last name: %s e-mail: %s", name, lastName, email);
+
+        String name = nameParts[0];
+        String lastname = String.join(" ", Arrays.copyOfRange(nameParts, 1, nameParts.length - 1));
+        String email = nameParts[nameParts.length - 1];
+
         if (!verifier.verifyName(name)) {
             System.out.println("Incorrect first name.");
             return false;
-        } else if (!verifier.verifyLastName(lastName)) {
+        } else if (!verifier.verifyLastName(lastname)) {
             System.out.println("Incorrect last name.");
             return false;
         } else if (!verifier.verifyEmail(email)) {
             System.out.println("Incorrect email.");
             return false;
         }
+        if (!isEmailUnique(email)) {
+            System.out.println("This email is already taken.");
+            return false;
+        }
+        studentResultsMap.put(new Student(name, lastname, email), new Courses(0, 0, 0, 0));
         return true;
     }
 
+    public static boolean isEmailUnique(String email) {
+        return studentResultsMap.keySet().stream()
+                .noneMatch(student -> student.getEmail().equals(email));
+    }
+
+    public static void showStudentsList() {
+        if (studentResultsMap.size() == 0) {
+            System.out.println("No students found");
+        } else {
+            System.out.println("Students: ");
+           studentResultsMap.keySet().forEach(System.out::println);
+        }
+    }
+
+    public static void addPoints() {
+        System.out.println("Enter an id and points or 'back' to return:");
+        boolean quit = false;
+        while (!quit) {
+            String inputPoints = scanner.nextLine().trim();
+            if ("back".equalsIgnoreCase(inputPoints)) {
+                quit = true;
+            } else {
+                if (isPointsValid(inputPoints)) {
+                    System.out.println("Points updated.");
+                }
+            }
+        }
+    }
+
+    public static boolean isPointsValid(String inputPoints) {
+        Verifier verifier = new Verifier();
+        String[] pointsData = inputPoints.split(" ");
+        String studentId = pointsData[0];
+        if (pointsData.length != 5) {
+            System.out.println("Incorrect points format.");
+            return false;
+        }
+        String[] points = Arrays.copyOfRange(pointsData, 1, pointsData.length);
+        Student studentById = findStudentById(studentId);
+
+        if (!verifier.validatePoints(points)) {
+            System.out.println("Incorrect points format.");
+            return false;
+        } else if (studentById == null) {
+            System.out.printf("No student is found for id=%s\n", studentId);
+            return false;
+        }
+        List<Integer> parsed = Arrays.stream(points).map(String::valueOf)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        Courses courses = studentResultsMap.get(studentById);
+        courses.updatePoints(parsed.get(0), parsed.get(1), parsed.get(2), parsed.get(3));
+        studentResultsMap.put(studentById, courses);
+        return true;
+    }
+
+    public static void findStudent() {
+        System.out.println("Enter an id or 'back' to return:");
+        boolean quit = false;
+        while (!quit) {
+            String studentId = scanner.nextLine().trim();
+            if ("back".equalsIgnoreCase(studentId)) {
+                quit = true;
+            } else {
+                Student studentById = findStudentById(studentId);
+                Courses courses = studentResultsMap.get(studentById);
+                System.out.println(studentById == null ? "No student is found for id=" + studentId
+                        : studentId + courses);
+            }
+        }
+    }
+
+    public static Student findStudentById(String id) {
+        return studentResultsMap.keySet().stream()
+                .filter(student -> student.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
 }
--- Learning Progress Tracker/task/src/tracker/Verifier.java
+++ Learning Progress Tracker/task/src/tracker/Verifier.java
@@ -1,13 +1,16 @@
 package tracker;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Verifier {
 
-    public  boolean verifyName(String name) {
+    public boolean verifyName(String name) {
         String nameRegex = "^[A-Za-z]+[-']?[A-Za-z]+";
         return name.matches(nameRegex);
     }
 
-    public  boolean verifyLastName(String lastName) {
+    public boolean verifyLastName(String lastName) {
         String lastNameRegex = "[A-Za-z ]+([-' ][A-Za-z ])*[-' ]?[A-Za-z]+";
         return lastName.matches(lastNameRegex);
     }
@@ -15,5 +18,11 @@ public class Verifier {
     public boolean verifyEmail(String email) {
         String pattern = "[\\w.]+@\\w+\\.[a-z\\d]+";
         return email.matches(pattern);
+    }
+
+    public boolean validatePoints(String[] points) {
+        List<String> list = Arrays.asList(points);
+        return list.stream().allMatch(s -> s.matches("\\d+"))
+                && list.stream().map(Integer::parseInt).allMatch(s -> s > 0);
     }
 }
